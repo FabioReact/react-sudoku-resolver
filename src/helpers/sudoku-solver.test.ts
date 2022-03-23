776194fb927c6818de205b320e323df8f3ae0116
FabioReact <fabio.react.dev@gmail.com>
2022-03-23 17:00:52 +0100

    feat: add checkValidNumbers, isEveryElementUnique, and findFirstCoordinate

--- src/helpers/sudoku-solver.test.ts
+++ src/helpers/sudoku-solver.test.ts
@@ -1,25 +1,69 @@
-import { extractNumbers, Shape } from "./sudoku-solver"
+import { extractNumbers, Shape, checkValidNumbers, isEveryElementUnique } from "./sudoku-solver"
+
+const sudokuArray = [
+	[0, 2, 0, 0, 0, 7, 0, 3, 0],
+	[0, 0, 0, 6, 0, 0, 1, 9, 7],
+	[9, 0, 0, 3, 0, 0, 2, 0, 4],
+	[0, 6, 0, 0, 5, 0, 8, 4, 0],
+	[0, 0, 8, 0, 2, 0, 3, 0, 0],
+	[0, 9, 4, 0, 6, 0, 0, 5, 0],
+	[6, 0, 2, 0, 0, 4, 0, 0, 1],
+	[3, 4, 9, 0, 0, 8, 0, 0, 0],
+	[0, 7, 0, 2, 0, 0, 0, 8, 0],
+]
 
 describe("extractNumbers", () => {
-	const sudokuArray = [
-		[0, 2, 0, 0, 0, 7, 0, 3, 0],
-		[0, 0, 0, 6, 0, 0, 1, 9, 7],
-		[9, 0, 0, 3, 0, 0, 2, 0, 4],
-		[0, 6, 0, 0, 5, 0, 8, 4, 0],
-		[0, 0, 8, 0, 2, 0, 3, 0, 0],
-		[0, 9, 4, 0, 6, 0, 0, 5, 0],
-		[6, 0, 2, 0, 0, 4, 0, 0, 1],
-		[3, 4, 9, 0, 0, 8, 0, 0, 0],
-		[0, 7, 0, 2, 0, 0, 0, 8, 0],
-	]
-
 	test("should return first line", () => {
 		const result = extractNumbers(sudokuArray, Shape.LINE, 0, 0)
 		expect(result).toEqual([0, 2, 0, 0, 0, 7, 0, 3, 0])
 	})
 
 	test("should return 9nth line", () => {
-		const result = extractNumbers(sudokuArray, Shape.LINE, 8, 0)
+		const result = extractNumbers(sudokuArray, Shape.LINE, 0, 8)
 		expect(result).toEqual([0, 7, 0, 2, 0, 0, 0, 8, 0])
+	})
+
+	test("should return first column", () => {
+		const result = extractNumbers(sudokuArray, Shape.COLUMN, 0, 0)
+		expect(result).toEqual([0, 0, 9, 0, 0, 0, 6, 3, 0])
+	})
+
+	test("should return 9nth colum", () => {
+		const result = extractNumbers(sudokuArray, Shape.COLUMN, 8, 0)
+		expect(result).toEqual([0, 7, 4, 0, 0, 0, 1, 0, 0])
+	})
+
+	test("should return first square", () => {
+		const result = extractNumbers(sudokuArray, Shape.SQUARE, 0, 0)
+		expect(result).toEqual([0, 2, 0, 0, 0, 0, 9, 0, 0])
+	})
+
+	test("should return 9nth square", () => {
+		const result = extractNumbers(sudokuArray, Shape.SQUARE, 8, 8)
+		expect(result).toEqual([0, 0, 1, 0, 0, 0, 0, 8, 0])
+	})
+})
+
+describe("checkValidNumbers", () => {
+	test("should return true if no duplicates (beside 0s)", () => {
+		const result = checkValidNumbers([9, 0, 0, 3, 0, 0, 2, 0, 4])
+		expect(result).toBe(true)
+	})
+
+	test("should return false if duplicates", () => {
+		const result = checkValidNumbers([9, 0, 0, 3, 0, 9, 2, 0, 4])
+		expect(result).toBe(false)
+	})
+})
+
+describe("isEveryElementUnique", () => {
+	test("should return false if array has duplicates", () => {
+		const result = isEveryElementUnique([9, 1, 4, 3, 5, 7, 2, 8, 4])
+		expect(result).toBe(false)
+	})
+
+	test("should return true if array has no duplicates", () => {
+		const result = isEveryElementUnique([9, 1, 4, 3, 5, 7, 2, 8, 6])
+		expect(result).toBe(true)
 	})
 })
--- src/helpers/sudoku-solver.ts
+++ src/helpers/sudoku-solver.ts
@@ -1,17 +1,47 @@
 enum Shape {
 	LINE,
 	COLUMN,
-	SQURE,
+	SQUARE,
+}
+
+const checkValidNumbers = (numbers: number[]): boolean => {
+	const posNumbers = numbers.filter(n => n > 0)
+	return new Set(posNumbers).size === posNumbers.length
+}
+
+const isEveryElementUnique = (numbers: number[]): boolean => {
+	return new Set(numbers).size === numbers.length
+}
+
+const findFirstCoordinate = (x: number): number => {
+	let firstX
+	if (x >= 6) firstX = 6
+	else if (x >= 3) firstX = 3
+	else firstX = 0
+	return firstX
 }
 
 const extractNumbers = (sudoku: number[][], shape: Shape, x: number, y: number): number[] => {
 	if (shape === Shape.LINE)
-		return sudoku[x]
+		return sudoku[y]
+	if (shape === Shape.COLUMN) {
+		const column = []
+		for (let index = 0; index < 9; index++)
+			column.push(sudoku[index][x])
+		return column
+	}
+	if (shape === Shape.SQUARE) {
+		const square = []
+		const firstX = findFirstCoordinate(x)
+		const firstY = findFirstCoordinate(y)
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				square.push(sudoku[i + firstY][j + firstX])
+			}
+		}
+		return square
+	}
 	return []
 }
 
-const verifyCombination = (line: number[]): boolean => {
-	return false
-}
-
-export { verifyCombination, extractNumbers, Shape }
+export { extractNumbers, isEveryElementUnique, checkValidNumbers, Shape }
